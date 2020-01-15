812226bca2316843260977ec245d47bc48c1c71e
unknown <kevankohjinwen@gmail.com>
2020-01-15 17:12:55 +0800

    added tabs for premium and budget

--- src/food.js
+++ src/food.js
@@ -25,62 +25,110 @@ const imgSatay = require('./imgs/satay.jpg')
 const imgWantonMee = require('./imgs/wantonmee.jpg')
 const imgKfc = require('./imgs/kfc.jpg')
 const imgBurgerKing = require('./imgs/burgerking.jpg')
+const imgThai = require('./imgs/padthai.jpg');
+const imgDtf = require('./imgs/dintaifung.jpg')
+const imgPasta = require('./imgs/pasta.jpg')
+const imgPizza = require('./imgs/pizza.jpg')
+const imgKoreanChicken = require('./imgs/koreanchicken.jpg');
+const imgRamen = require('./imgs/ramen.jpg')
+const imgKoreanBbq = require('./imgs/koreanbbq.jpg')
+const imgGerman = require('./imgs/german.jpg')
+const imgVietnamese = require('./imgs/vietnamese.jpg')
+const imgSteak = require('./imgs/steak.jpg')
+const imgBurger = require('./imgs/burger.jpg');
+const imgPorridge = require('./imgs/porridge.jpg')
+const imgLjs = require('./imgs/ljs.jpg');
+const imgSubway = require('./imgs/subway.jpg')
+const imgSlicedFishSoup = require('./imgs/slicedfishsoup.jpg')
+
 
 //default cases
 const sushi = {
     name: 'Sushi',
-    img: imgSushi
+    img: imgSushi,
+    atas: 1
 }
 
 const friedRice = {
     name: 'Fried Rice',
-    img: imgfriedRice
+    img: imgfriedRice,
+    atas: 0
 }
 
 const laksa = {
     name: 'Laksa',
-    img: imgLaksa
+    img: imgLaksa,
+    atas: 0
 }
 
 const bcm = {
     name: 'Bak Chor Mee',
-    img: imgBcm
+    img: imgBcm,
+    atas: 0
 }
 
 //creating foodFactory
-const foodFactory = (name, img) => {
+const foodFactory = (name, img, atas) => {
     return  {
         name : name,
-        img: img
+        img: img,
+        atas: atas
     }
 }
-const asian = [sushi, friedRice, laksa, bcm]
+const asian = [sushi, friedRice, laksa, bcm];
+const atas = [];
+const cheap = [];
 
-asian.push(foodFactory('Bak Kut Teh', imgBkt))
-asian.push(foodFactory('Ban Mian', imgbanMian))
-asian.push(foodFactory('Char Kway Teow', imgCharKt))
-asian.push(foodFactory('Chicken Rice', imgChickenRice))
-asian.push(foodFactory('Mcdonald\'s', imgMacs))
-asian.push(foodFactory('Duck Rice', imgDuckRice))
-asian.push(foodFactory('Bee Hoon Soup', imgBeeHoon))
-asian.push(foodFactory('Carrot Cake', imgCarrotCake))
-asian.push(foodFactory('Cai Fan', imgCaiFan))
-asian.push(foodFactory('Hokkien Mee', imgHokkienMee))
-asian.push(foodFactory('Kway Chap', imgKwayChap))
-asian.push(foodFactory('Mala Xiang Guo', imgMala))
-asian.push(foodFactory('Mee Rebus', imgMeeRebus))
-asian.push(foodFactory('Mee Siam', imgMeeSiam))
-asian.push(foodFactory('Nasi Lemak', imgNasiLemak))
-asian.push(foodFactory('Oyster Omelette', imgOysterOm))
-asian.push(foodFactory('Prawn Mee', imgPrawnMee))
-asian.push(foodFactory('Roti Prata', imgRotiPrata))
-asian.push(foodFactory('Zi Char', imgZiChar))
-asian.push(foodFactory('Satay', imgSatay))
-asian.push(foodFactory('Wanton Mee', imgWantonMee))
-asian.push(foodFactory('Kfc', imgKfc))
-asian.push(foodFactory('Burger King', imgBurgerKing))
+asian.push(foodFactory('Bak Kut Teh', imgBkt, 0))
+asian.push(foodFactory('Ban Mian', imgbanMian, 0))
+asian.push(foodFactory('Char Kway Teow', imgCharKt, 0))
+asian.push(foodFactory('Chicken Rice', imgChickenRice, 0))
+asian.push(foodFactory('Mcdonald\'s', imgMacs, 0))
+asian.push(foodFactory('Duck Rice', imgDuckRice, 0))
+asian.push(foodFactory('Bee Hoon Soup', imgBeeHoon, 0))
+asian.push(foodFactory('Carrot Cake', imgCarrotCake, 0))
+asian.push(foodFactory('Cai Fan', imgCaiFan, 0))
+asian.push(foodFactory('Hokkien Mee', imgHokkienMee, 0))
+asian.push(foodFactory('Kway Chap', imgKwayChap, 0))
+asian.push(foodFactory('Mala Xiang Guo', imgMala, 0))
+asian.push(foodFactory('Mee Rebus', imgMeeRebus, 0))
+asian.push(foodFactory('Mee Siam', imgMeeSiam, 0))
+asian.push(foodFactory('Nasi Lemak', imgNasiLemak, 0))
+asian.push(foodFactory('Oyster Omelette', imgOysterOm, 0))
+asian.push(foodFactory('Prawn Mee', imgPrawnMee, 0))
+asian.push(foodFactory('Roti Prata', imgRotiPrata, 0))
+asian.push(foodFactory('Zi Char', imgZiChar, 0))
+asian.push(foodFactory('Satay', imgSatay, 0))
+asian.push(foodFactory('Wanton Mee', imgWantonMee, 0))
+asian.push(foodFactory('KFC', imgKfc, 0))
+asian.push(foodFactory('Burger King', imgBurgerKing, 0))
+asian.push(foodFactory('Thai', imgThai, 1))
+asian.push(foodFactory('Dim Sum', imgDtf, 1))
+asian.push(foodFactory('Italian Pasta', imgPasta, 1))
+asian.push(foodFactory('Pizza', imgPizza, 1))
+asian.push(foodFactory('Korean Fried Chicken', imgKoreanChicken, 1))
+asian.push(foodFactory('Ramen', imgRamen, 1))
+asian.push(foodFactory('Korean BBQ', imgKoreanBbq, 1))
+asian.push(foodFactory('German Sausages', imgGerman, 1))
+asian.push(foodFactory('Vietnamese', imgVietnamese, 1))
+asian.push(foodFactory('Steak', imgSteak, 1))
+asian.push(foodFactory('Premium Burgers', imgBurger, 1))
+asian.push(foodFactory('Porridge', imgPorridge, 0))
+asian.push(foodFactory('Subway', imgSubway, 0))
+asian.push(foodFactory('Long John\'s Silver', imgLjs, 0))
+asian.push(foodFactory('Sliced Fish Soup', imgSlicedFishSoup, 0))
 
+asian.forEach(element => {
+    if (element.atas === 1) {
+        atas.push(element)
+    }
+});
 
+asian.forEach(element => {
+    if (element.atas === 0){
+        cheap.push(element);
+    }
+})
 
-module.exports = asian;
+export {asian, atas, cheap};
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Asian from './food.js'
-import Blank from './imgs/Blank.jpg'
+import {asian, atas, cheap} from './food.js';
+import Blank from './imgs/Blank.jpg';
+import { findAllByAltText } from '@testing-library/react';
 
 
 class Header extends React.Component {
@@ -46,6 +47,46 @@ class Button extends React.Component {
     }
 }
 
+class Tab extends React.Component {
+    renderSwitch(param){
+        switch(param) {
+            case 'all':
+                return <ul className = 'tab'>
+                        <li id = 'active'>All</li>
+                        <li className='middleList' onClick = {this.props.changeTab2}>Atas</li>
+                        <li onClick = {this.props.changeTabCheap}>Not so Atas</li>
+                     </ul>;
+            case 'atas':
+                return <ul className = 'tab'>
+                <li onClick = {this.props.changeTab}>All</li>
+                <li className='middleList' id = 'active'>Atas</li>
+                <li onClick = {this.props.changeTabCheap}>Not so Atas</li>
+                </ul>;
+            case 'cheap':
+                return <ul className = 'tab'>
+                <li onClick = {this.props.changeTab}>All</li>
+                <li onClick = {this.props.changeTab2} className='middleList'>Atas</li>
+                <li id = 'active'>Not so Atas</li>
+                </ul>;
+            default: 
+            return <ul className = 'tab'>
+                        <li id = 'active'>All</li>
+                        <li className='middleList' onClick = {this.props.changeTab2}>Atas</li>
+                        <li onClick = {this.props.changeTabCheap}>Not so Atas</li>
+                     </ul>;
+        } 
+    }
+
+    render(){
+        return (
+            <div>
+            {this.renderSwitch(this.props.tabState)
+            }
+            </div>
+        )
+    }
+}
+
 class FoodType extends React.Component {
     constructor(props){
         super(props);
@@ -57,52 +98,111 @@ class FoodType extends React.Component {
             word: 'Eat what?',
             question: '',
             end: false,
-            repeated: []
+            repeated: [],
+            tab: 'all',
+            table: asian
         }
     }
 
-    generateFood(Asian, random){
+    generateFood(table, random){
         const words = ['Why not have some ', 'How about ', 'Try ', 'Maybe you can consider ', 'Could you be craving ', 'Maybe you would like ']
         const word = Math.floor(Math.random() * words.length);
         return this.setState({
             random: random,
-            food: Asian[random].name, 
-            img: Asian[random].img,
+            food: table[random].name, 
+            img: table[random].img,
             word: words[word],
             question: '?'
             })
     }
 
-    resetState(){
+    resetState() {
+        const sentAll = ['Hungry hungry very very...', 'A hungry man is an angry man', 'I can eat a cow right now...', 'There is no love sincerer than the love of food.']
+        const randSentAll = sentAll[Math.floor(Math.random() * sentAll.length)] 
         this.setState({
             random: '',
             food: '',
             img: Blank,
-            word: 'Hungry hungry very very...',
+            word: randSentAll,
             question: '',
             end: false,
-            repeated: []
-    })
-}
+            repeated: [],
+            tab: 'all',
+            table: asian
+        })
+    }
 
-    render(){
-        let random = Math.floor(Math.random() * Asian.length)
+    changeTab1(){
+        const sentAll = ['Hungry hungry very very...', 'A hungry man is an angry man...', 'I can eat a cow right now...', 'There is no love sincerer than the love of food.']
+        const randSentAll = sentAll[Math.floor(Math.random() * sentAll.length)] 
+        this.setState({
+            random: '',
+            food: '',
+            img: Blank,
+            word: randSentAll,
+            question: '',
+            end: false,
+            repeated: [],
+            tab: 'all',
+            table: asian
+        })
+    }
+
+    changeTab2(){
+        const fancyAll = ['Oooh.. Fancy Fancy', 'Earn Money Spend Money', 'A little splurge is alright once in a while..', 'Let food be thy medicine and medicine be thy food.', 'What is money if you don\'t spend it on food?', 'Good Food Incoming...']
+        const randFancyAll = fancyAll[Math.floor(Math.random() * fancyAll.length)]
+        this.setState({
+            random: '',
+            food: '',
+            img: Blank,
+            word: randFancyAll,
+            question: '',
+            end: false,
+            repeated: [],
+            tab: 'atas',
+            table: atas
+        })
+    }
+
+    changeTabCheap(){
+        const sentAll = ['Hungry hungry very very...', 'A hungry man is an Angry Man...', 'I can eat a cow right now...', 'Food does not need to be expensive to be good!']
+        const randSentAll = sentAll[Math.floor(Math.random() * sentAll.length)];
+        this.setState({
+            random: '',
+            food: '',
+            img: Blank,
+            word: randSentAll,
+            question: '',
+            end: false,
+            repeated: [],
+            tab: 'cheap',
+            table: cheap
+        }) 
+    }
+
+    randomise(){
+        let random = Math.floor(Math.random() * this.state.table.length)
         this.state.repeated.push(this.state.random);
-        if (this.state.repeated.length === Asian.length){
+        if (this.state.repeated.length === this.state.table.length){
             this.setState({end: true})
         }
 
         if(!this.state.end){
         while (this.state.repeated.includes(random)){
-            random = Math.floor(Math.random() * Asian.length)
+            random = Math.floor(Math.random() * this.state.table.length)
         }}
+        return random;
+    }
 
+    render(){
         return (
             <div className = 'foodType'>
                 <Food />
+                <Tab changeTab = {() => this.changeTab1()} changeTab2= {()=> this.changeTab2()} changeTabCheap = {() => this.changeTabCheap()}tabState = {this.state.tab}/>
                 <p className = 'foodName'>{this.state.word + this.state.food + this.state.question}</p>
                 <img src = {this.state.img} alt = 'food' className = 'foodWrapper'></img>
-                <Button onClick={() => this.generateFood(Asian, random)} 
+                <Button onClick={() => {
+                    this.generateFood(this.state.table, this.randomise())}} 
                     hasEnded={this.state.end} 
                     onClick2={() => this.resetState()
                     }/>
