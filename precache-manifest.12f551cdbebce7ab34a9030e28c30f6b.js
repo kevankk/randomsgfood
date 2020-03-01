e355f98f5cc329f4ac3c71f57ead30b5a662ff71
Kevan Koh <kevankohjinwen@gmail.com>
2020-03-01 10:36:55 +0700

    Updates

---
+++ precache-manifest.12f551cdbebce7ab34a9030e28c30f6b.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64eb0907fc882ff131246d1411664024",
+    "url": "/randomsgfood/index.html"
+  },
+  {
+    "revision": "019f7daed353db37a92f",
+    "url": "/randomsgfood/static/css/main.61e1fd20.chunk.css"
+  },
+  {
+    "revision": "a6257767b07a050c4660",
+    "url": "/randomsgfood/static/js/2.eb5eefe4.chunk.js"
+  },
+  {
+    "revision": "e88c7c35e33fe2f07d6d55a39dab85ff",
+    "url": "/randomsgfood/static/js/2.eb5eefe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "019f7daed353db37a92f",
+    "url": "/randomsgfood/static/js/main.c0dfb146.chunk.js"
+  },
+  {
+    "revision": "6209d152641957121555",
+    "url": "/randomsgfood/static/js/runtime-main.0d117509.js"
+  },
+  {
+    "revision": "cf5b33a4a2147d9db75ae33414420a3a",
+    "url": "/randomsgfood/static/media/Sushi.cf5b33a4.jpg"
+  },
+  {
+    "revision": "2eceeef0d8cbbd5cca2eaf99d5bc5b41",
+    "url": "/randomsgfood/static/media/background2.2eceeef0.jpg"
+  },
+  {
+    "revision": "94368adfc88571183a82f2dc4375a743",
+    "url": "/randomsgfood/static/media/banmian.94368adf.jpg"
+  },
+  {
+    "revision": "df6a8589f36de5b9cef88ca6966920a3",
+    "url": "/randomsgfood/static/media/bcm.df6a8589.jpg"
+  },
+  {
+    "revision": "9317693650a4b2ab5af8b7e70b5a3914",
+    "url": "/randomsgfood/static/media/beehoon.93176936.jpg"
+  },
+  {
+    "revision": "b24f33452a574b0f33723c9cb23c5c9d",
+    "url": "/randomsgfood/static/media/bkt.b24f3345.jpg"
+  },
+  {
+    "revision": "41d93dce00cb028967f9c73d5e5f6cec",
+    "url": "/randomsgfood/static/media/burger.41d93dce.jpg"
+  },
+  {
+    "revision": "d30c54326457c4e32f8b8050e5ba646e",
+    "url": "/randomsgfood/static/media/burgerking.d30c5432.jpg"
+  },
+  {
+    "revision": "bb3b142b27e694e4960c9a666c3d499d",
+    "url": "/randomsgfood/static/media/caifan.bb3b142b.jpg"
+  },
+  {
+    "revision": "829fb9dfc6c4a1c2a4682ed6280e1d97",
+    "url": "/randomsgfood/static/media/carrotcake.829fb9df.jpg"
+  },
+  {
+    "revision": "25f082376466ee60ef8390fae3db745c",
+    "url": "/randomsgfood/static/media/charkwayteow.25f08237.jpg"
+  },
+  {
+    "revision": "884d65e3c326253fe8b8757a09af5e7f",
+    "url": "/randomsgfood/static/media/chickenrice.884d65e3.jpg"
+  },
+  {
+    "revision": "e1567ad8910cc8eedc5d5fa6aa036545",
+    "url": "/randomsgfood/static/media/dintaifung.e1567ad8.jpg"
+  },
+  {
+    "revision": "a27986570e9c15b6f889d3672f3a198a",
+    "url": "/randomsgfood/static/media/duckrice.a2798657.jpg"
+  },
+  {
+    "revision": "41f8e06c0e8774740c9f3dfb8e3431ea",
+    "url": "/randomsgfood/static/media/friedrice.41f8e06c.jpg"
+  },
+  {
+    "revision": "d6e7276af79af5e209123b6bc5384b57",
+    "url": "/randomsgfood/static/media/german.d6e7276a.jpg"
+  },
+  {
+    "revision": "ec856dbd781ef939e15ac2605df4c214",
+    "url": "/randomsgfood/static/media/hokkienmee.ec856dbd.jpg"
+  },
+  {
+    "revision": "787843ef4d4798abb36cc95f0ba22402",
+    "url": "/randomsgfood/static/media/kfc.787843ef.jpg"
+  },
+  {
+    "revision": "bf6795f71b29cbbf08ea9d252167a7b7",
+    "url": "/randomsgfood/static/media/koreanbbq.bf6795f7.jpg"
+  },
+  {
+    "revision": "7ec4e42cca2db0864a5475745360202e",
+    "url": "/randomsgfood/static/media/koreanchicken.7ec4e42c.jpg"
+  },
+  {
+    "revision": "00129e6b11e0da3b38212858e3e78518",
+    "url": "/randomsgfood/static/media/kwaychap.00129e6b.jpg"
+  },
+  {
+    "revision": "f712061940b155a2b45b2c02e6c96c74",
+    "url": "/randomsgfood/static/media/laksa.f7120619.jpg"
+  },
+  {
+    "revision": "4cd3f2a85accb2873dd0f289a563d783",
+    "url": "/randomsgfood/static/media/ljs.4cd3f2a8.jpg"
+  },
+  {
+    "revision": "7a90aab8973bde0b1b6b49cee625a3e0",
+    "url": "/randomsgfood/static/media/mala.7a90aab8.jpg"
+  },
+  {
+    "revision": "d2fef7bcc291c9c7d5b4def4a18b6c98",
+    "url": "/randomsgfood/static/media/mcdonalds.d2fef7bc.jpg"
+  },
+  {
+    "revision": "fe5b5ce3850d77dfacc31556aebc779e",
+    "url": "/randomsgfood/static/media/meerebus.fe5b5ce3.jpg"
+  },
+  {
+    "revision": "ddc18d4c0429b70cb97c2458e92d8cc8",
+    "url": "/randomsgfood/static/media/meesiam.ddc18d4c.jpeg"
+  },
+  {
+    "revision": "311e79c843e941e36050cc14cbb486bd",
+    "url": "/randomsgfood/static/media/nasilemak.311e79c8.jpg"
+  },
+  {
+    "revision": "2230d6abc9d22dfab6e50fbde4f89564",
+    "url": "/randomsgfood/static/media/oysterom.2230d6ab.jpg"
+  },
+  {
+    "revision": "8e1c2a5e13751fb5b71efdf3fd27959c",
+    "url": "/randomsgfood/static/media/padthai.8e1c2a5e.jpg"
+  },
+  {
+    "revision": "aa52d84de9143c9502b48c9764556f79",
+    "url": "/randomsgfood/static/media/pizza.aa52d84d.jpg"
+  },
+  {
+    "revision": "f1ee7cc51ad46d88f16ad4cc870a52c9",
+    "url": "/randomsgfood/static/media/prawnmee.f1ee7cc5.jpg"
+  },
+  {
+    "revision": "3dbfd83228232653c8d06983e68b7673",
+    "url": "/randomsgfood/static/media/ramen.3dbfd832.jpg"
+  },
+  {
+    "revision": "2141f2c07af52e4713a48dbe73079b19",
+    "url": "/randomsgfood/static/media/rotiprata.2141f2c0.jpg"
+  },
+  {
+    "revision": "f4107e4312a38f4e5f34c974113a6b0e",
+    "url": "/randomsgfood/static/media/satay.f4107e43.jpg"
+  },
+  {
+    "revision": "f0dded9e5987fd4d8221a923cb97b039",
+    "url": "/randomsgfood/static/media/slicedfishsoup.f0dded9e.jpg"
+  },
+  {
+    "revision": "6110572f84ae804675f0e66e9bd53301",
+    "url": "/randomsgfood/static/media/steak.6110572f.jpg"
+  },
+  {
+    "revision": "d29fe607020dcab4795991c04c706d57",
+    "url": "/randomsgfood/static/media/subway.d29fe607.jpg"
+  },
+  {
+    "revision": "ecf0626559f95dceaf116f50ce295f96",
+    "url": "/randomsgfood/static/media/vietnamese.ecf06265.jpg"
+  },
+  {
+    "revision": "de465e447b72e2724812a2669630ec87",
+    "url": "/randomsgfood/static/media/wantonmee.de465e44.jpg"
+  },
+  {
+    "revision": "2329e8a415d5598e421f9b9148078408",
+    "url": "/randomsgfood/static/media/zichar.2329e8a4.jpg"
+  }
+]);
